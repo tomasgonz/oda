d24493b4d3fba8b20392422f6f45af789c840de5
Tomas Gonzalez <tomasgonz@gmail.com>
2022-10-30 22:34:02 -0400

    Changes to recipient analysis

--- oda_recipients.py
+++ oda_recipients.py
@@ -1,6 +1,7 @@
 import colorsys
 from re import S
 import os
+from this import d
 from webbrowser import get
 import streamlit as st
 import pandas as pd
@@ -28,23 +29,10 @@ donors = data['Donor'].unique()
 years = data['Year'].unique()
 series = data['Series'].unique()
 recipients = data['Recipient'].unique()
-recipients = np.insert(recipients, 0, "all recipients")
-
-#data = data[~data['Recipient'].str.contains('Total')]
-#data = data[~data['Recipient'].str.contains('regional')]
-#data = data[~data['Recipient'].str.contains('WorldBank')]
-#data = data[~data['Recipient'].str.contains('World Bank')]
-#data = data[~data['Recipient'].str.contains('World Bank')]
-#data = data[~data['Recipient'].str.contains('Part')]
 
 selected_donor = st.sidebar.selectbox('Select a donor', donors)
 if selected_donor != "all donors":
         data = data[data['Donor'] == selected_donor]
-
-selected_recipient = st.sidebar.selectbox('Select a recipient', recipients)
-if selected_recipient:
-    if selected_recipient != "all recipients":
-        data = data[data['Recipient'] == selected_recipient]
 
 selected_series = st.sidebar.selectbox('Select series', series)
 if selected_series:
@@ -72,44 +60,62 @@ fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'Europe, Total'], 'Euro
 fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'Middle East, Total'], 'Middle East, Total'))
 fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'America, Total'], 'America, Total'))
 
-
 fig_yearly.update_layout(title='ODA to Developing Countries', xaxis_title='Year', 
     yaxis_title='ODA (USD)', height=600)
 
 st.plotly_chart(fig_yearly, use_container_width=True)
 
 # 2020
-data_2020 = data[data['Year'] == 2020].groupby(['Donor', 'Recipient'], as_index = False).sum()
-labels =  data_2020['Donor'].unique().tolist() + recipients.tolist()
+data = data[data['Year'] == 2019].groupby(['Donor', 'Recipient'], as_index = False).sum()
+data.sort_values(by=['Value'], inplace=True, ascending=False)
 
-index_source = []
-for e in data_2020['Donor'].values.tolist():
-    index_source.append(labels.index(e))
+labels = data['Recipient'].unique().tolist()
 
-index_destination = []
-for e in data_2020['Recipient'].values.tolist():
-    index_destination.append(labels.index(e))
+parents = [selected_donor] * len(labels)
 
-st.header("How is " + selected_donor + " allocating its aid to " + selected_recipient + " by sector in 2020?")
+st.header("How is " + selected_donor + " allocating its aid by country or region?")
 st.write("The size of the arrows represents the amount of aid allocated to each recipient. The color of the arrows represents the sector of the aid. The size of the nodes represents the total amount of aid received by each recipient. The color of the nodes represents the region of the recipient.")
 
 fig = go.Figure(data=[go.Treemap(
     labels=labels,
-    parents= index_destination,
-    values=data_2020['Value'].values.tolist(),
-    branchvalues="total",
-    marker=dict(
-        colors=['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'] * 2,
-        line=dict(width=2, color='#000000')
-    ),
-    textinfo='label+value',
-    hovertemplate = '%{label}<extra></extra>',
-    pathbar=dict(visible=False),
-    maxdepth=2
+    parents=parents,
+    values=data['Value']
 )])
 
-fig.update_layout(height=1500)
+fig.update_layout(height=800)
 
 st.plotly_chart(fig, font_size=10, use_container_width=True, height=2000)
 
-st.header("How have the trends of recipients of aid by " + selected_donor + " to " + selected_recipient + " evolved from 2010 to 2020?")
+st.dataframe(data[['Donor', 'Recipient', 'Value']], use_container_width=True)
+
+st.header("How is " + selected_donor + " allocating its aid by country?")
+
+data = data[~data['Recipient'].str.contains('Total')]
+data = data[~data['Recipient'].str.contains('regional')]
+data = data[~data['Recipient'].str.contains('WorldBank')]
+data = data[~data['Recipient'].str.contains('World Bank')]
+data = data[~data['Recipient'].str.contains('World Bank')]
+data = data[~data['Recipient'].str.contains('Part')]
+
+# 2020
+data = data[data['Year'] == 2019].groupby(['Donor', 'Recipient'], as_index = False).sum()
+data.sort_values(by=['Value'], inplace=True, ascending=False)
+
+labels = data['Recipient'].unique().tolist()
+
+parents = [selected_donor] * len(labels)
+
+st.header("How is " + selected_donor + " allocating its aid by country or region?")
+st.write("The size of the arrows represents the amount of aid allocated to each recipient. The color of the arrows represents the sector of the aid. The size of the nodes represents the total amount of aid received by each recipient. The color of the nodes represents the region of the recipient.")
+
+fig = go.Figure(data=[go.Treemap(
+    labels=labels,
+    parents=parents,
+    values=data['Value']
+)])
+
+fig.update_layout(height=800)
+
+st.plotly_chart(fig, font_size=10, use_container_width=True, height=2000)
+
+st.dataframe(data[['Donor', 'Recipient', 'Value']], use_container_width=True)
