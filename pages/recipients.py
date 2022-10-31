2d46021e3ed2e5e288dcce21b4857593f0f6e450
Tomas <tomasgonz@gmail.com>
2022-10-31 15:02:41 -0400

    Added selection of recipient and Sankey chart

--- pages/recipients.py
+++ pages/recipients.py
@@ -26,13 +26,21 @@ years = data['Year'].unique()
 series = data['Series'].unique()
 recipients = data['Recipient'].unique()
 
+data_selected_donor = data
+
 selected_donor = st.sidebar.selectbox('Select a donor', donors)
 if selected_donor != "all donors":
-        data = data[data['Donor'] == selected_donor]
+        data_selected_donor = data_selected_donor[data_selected_donor['Donor'] == selected_donor]
 
 selected_series = st.sidebar.selectbox('Select series', series)
 if selected_series:
-    data = data[data['Series'] == selected_series]
+    data_selected_donor = data_selected_donor[data_selected_donor['Series'] == selected_series]
+
+recipients =  np.insert(recipients, 0, "all recipients")
+
+selected_recipient = st.sidebar.selectbox('Select recipient', recipients)
+if selected_recipient != "all recipients":
+    data_selected_donor = data_selected_donor[data_selected_donor['Recipient'] == selected_recipient]
 
 # ODA evolution
 st.header("An overview of the distribution of aid of " + selected_donor)
@@ -42,19 +50,22 @@ st.write("The chart below represets " + selected_series +
 
 fig_yearly = go.Figure()
 
-def get_trace(data, name):
-    return go.Scatter(x=data['Year'], y=data['Value'], name=name, fill='tozeroy')
+def get_trace(data_selected_donor, name):
+    return go.Scatter(x=data_selected_donor['Year'], y=data_selected_donor['Value'], name=name, fill='tozeroy')
 
-fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'Developing Countries, Total'], 
+fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == 'Developing Countries, Total'], 
     'Developing Countries, Total'))
-fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'LDCs, Total'], 'LDCs, Total'))
-fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'Africa, Total'], 'Africa, Total'))
-fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'Asia, Total'], 'Asia, Total'))
-fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'LMICs, Total'], 'LMICs, Total'))
-fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'UMICs, Total'], 'UMICs, Total'))
-fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'Europe, Total'], 'Europe, Total'))
-fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'Middle East, Total'], 'Middle East, Total'))
-fig_yearly.add_trace(get_trace(data[data['Recipient'] == 'America, Total'], 'America, Total'))
+fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == 'LDCs, Total'], 'LDCs, Total'))
+fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == 'Africa, Total'], 'Africa, Total'))
+fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == 'Asia, Total'], 'Asia, Total'))
+fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == 'LMICs, Total'], 'LMICs, Total'))
+fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == 'UMICs, Total'], 'UMICs, Total'))
+fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == 'Europe, Total'], 'Europe, Total'))
+fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == 'Middle East, Total'], 'Middle East, Total'))
+fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == 'America, Total'], 'America, Total'))
+
+if selected_recipient:
+    fig_yearly.add_trace(get_trace(data_selected_donor[data_selected_donor['Recipient'] == selected_recipient], selected_recipient))
 
 fig_yearly.update_layout(title='ODA to Developing Countries', xaxis_title='Year', 
     yaxis_title='ODA (USD)', height=600)
@@ -62,8 +73,8 @@ fig_yearly.update_layout(title='ODA to Developing Countries', xaxis_title='Year'
 st.plotly_chart(fig_yearly, use_container_width=True)
 
 # 2020
-data = data[data['Year'] == 2019].groupby(['Donor', 'Recipient'], as_index = False).sum()
-data.sort_values(by=['Value'], inplace=True, ascending=False)
+data_2020 = data[data['Year'] == 2020].groupby(['Donor', 'Recipient'], as_index = False).sum()
+data_2020.sort_values(by=['Value'], inplace=True, ascending=False)
 
 labels = data['Recipient'].unique().tolist()
 
@@ -82,7 +93,7 @@ fig.update_layout(height=800)
 
 st.plotly_chart(fig, font_size=10, use_container_width=True, height=2000)
 
-st.dataframe(data[['Donor', 'Recipient', 'Value']], use_container_width=True)
+st.dataframe(data_2020[['Donor', 'Recipient', 'Value']], use_container_width=True)
 
 st.header("How is " + selected_donor + " allocating its aid by country?")
 st.write("The same as above, but excluding country group aggregates and showing only individual countries. Note that for some countries a substantial volume of aid is reported as unspecified.")
@@ -95,9 +106,7 @@ data = data[~data['Recipient'].str.contains('World Bank')]
 data = data[~data['Recipient'].str.contains('Part')]
 data = data[~data['Recipient'].str.contains('unspecified')]
 
-# 2020
-data = data[data['Year'] == 2019].groupby(['Donor', 'Recipient'], as_index = False).sum()
-data.sort_values(by=['Value'], inplace=True, ascending=False)
+data_2020.sort_values(by=['Value'], inplace=True, ascending=False)
 
 labels = data['Recipient'].unique().tolist()
 
@@ -114,3 +123,59 @@ fig.update_layout(height=800)
 st.plotly_chart(fig, font_size=10, use_container_width=True, height=2000)
 
 st.dataframe(data[['Donor', 'Recipient', 'Value']], use_container_width=True)
+
+if selected_recipient == 'all recipients':
+    st.stop()
+
+st.caption("""Beta ODA explorer development by Tomas Gonzalez using data from the OECD at https://stats.oecd.org/. 
+
+This app is based on Streamlit and the source code is available at https://github.com/tomasgonz/oda. The source code is free to use, modify and distribute.""")
+
+st.title("Aid (ODA) by sector and donor")
+
+st.write(""" Official Development Assistance (ODA) is defined as those flows to developing countries and multilateral institutions provided by official agencies, including state and local governments, or by their executive agencies, each transaction of which meets the following tests: i) it is administered with the promotion of the economic development and welfare of developing countries as its main objective; and ii) it is concessional in character and conveys a grant element of at least 25 per cent. The data is in millions, current US. 
+
+OECD (2022), "Data warehouse", OECD.Stat (database), https://doi.org/10.1787/data-00900-en (accessed on 12 October 2022). """
+)
+
+# 2020 - Recipients
+
+data_2020 = data[data['Year'] == 2020].groupby(['Donor', 'Recipient', 'Year'], as_index = False)['Value'].sum()
+
+data_2020 = data_2020[data_2020['Recipient'] == selected_recipient]
+
+data_2020 = data_2020[~data_2020['Donor'].str.contains('Official Donors')]
+
+labels = data_2020['Donor'].unique().tolist() + recipients.tolist()
+
+index_source = []
+for e in data_2020['Donor'].values.tolist():
+    index_source.append(labels.index(e))
+
+index_destination = []
+for e in data_2020['Recipient'].values.tolist():
+    index_destination.append(labels.index(e))
+
+st.header("How is " + selected_donor + " allocating its aid by recipient in 2020?")
+fig = go.Figure(data=[go.Sankey(
+    node = {
+        'pad' : 15,
+        'thickness' : 20,
+        'line' : {
+            'color' : 'black', 
+            'width' : 0.5
+        },
+        'label' :labels
+    },
+    link = {
+        'source' : index_source,
+        'target' : index_destination,
+        'value' : data_2020['Value']
+    }
+)])
+
+fig.update_layout(height=1000)
+
+st.plotly_chart(fig, font_size=10, use_container_width=True, height=1000)
+
+st.dataframe(data_2020[['Donor', 'Recipient', "Value"]].sort_values(by=['Value'], ascending=False), use_container_width=True)
