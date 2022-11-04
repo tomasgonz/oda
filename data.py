3a08497bf1eeeb2960c00949bfaf9c7443ca86a6
Tomas <tomasgonz@gmail.com>
2022-11-04 10:01:13 -0400

    change to new folder in origin

--- data.py
+++ data.py
@@ -1,12 +1,12 @@
 from cache import load_file
 def get_oda_recipients():    
-    return load_file("https://datasource.nyc3.digitaloceanspaces.com/DACGEO.csv", "DACGEO.csv")
+    return load_file("https://datasource.nyc3.digitaloceanspaces.com/oecd/DACGEO.csv", "DACGEO.csv")
 
 def get_oda_volume(): 
-    return load_file("https://datasource.nyc3.digitaloceanspaces.com/TABLE1_export.csv", "TABLE1_export.csv")
+    return load_file("https://datasource.nyc3.digitaloceanspaces.com/oecd/TABLE1_export.csv", "TABLE1_export.csv")
 
 def get_oda_sectors(): 
-    return load_file("https://datasource.nyc3.digitaloceanspaces.com/TABLE5_export.csv", "TABLE5_export.csv")
+    return load_file("https://datasource.nyc3.digitaloceanspaces.com/oecd/TABLE5_export.csv", "TABLE5_export.csv")
 
 def get_crs():
-    return load_file("https://datasource.nyc3.digitaloceanspaces.com/CRS2020data.csv", "CRS2020data.csv")
+    return load_file("https://datasource.nyc3.digitaloceanspaces.com/oecd/CRS2020data.csv", "CRS2020data.csv")
