ea6389c3b835c76d064f0a68ff48aae623fc11c8
Tomas Gonzalez <tomasgonz@gmail.com>
2022-11-04 19:06:26 -0400

    Changed titles of group based analysis charts

--- pages/Group analysis.py
+++ pages/Group analysis.py
@@ -1,15 +1,10 @@
-import colorsys
 from re import S
-import os
 from webbrowser import get
 import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
 import plotly.graph_objects as go
-import random
-import urllib
-import importlib
 from data import get_oda_recipients
 from paises import Countries
 from paises import Groups
@@ -18,15 +13,16 @@ from paises.alias import get_alias
 st.set_page_config(layout="wide")
 
 data = get_oda_recipients()
-series = data['Series'].unique()
-selected_series = st.sidebar.selectbox('Select series', series)
-if selected_series:
-    data = data[data['Series'] == selected_series]
 
 donors = data['Donor'].unique()
 selected_donor = st.sidebar.selectbox('Select a donor', donors)
 if selected_donor != "all donors":
         data = data[data['Donor'] == selected_donor]
+
+series = data['Series'].unique()
+selected_series = st.sidebar.selectbox('Select series', series)
+if selected_series:
+    data = data[data['Series'] == selected_series]
 
 def get_trace(data, name):
     return go.Scatter(x=data['Year'], y=data['Value'], name=name)
@@ -63,11 +59,12 @@ for dg in data_groups:
     fig_groups.add_trace(get_trace(dg['data'].groupby(['Year'], as_index = False)['Value'].sum(), dg['acronym']))
 
 if selected_groups:
-    fig_groups.update_layout(title='ODA to Developing Countries', xaxis_title='Year', 
+    st.header(selected_series + ' to ' + ', '.join(selected_groups))
+    fig_groups.update_layout(xaxis_title='Country', 
         yaxis_title='ODA (USD)', height=600)
     st.plotly_chart(fig_groups, use_container_width=True)
 else:
-    st.write("Select a group to see the distribution of aid")
+    st.write(" <- Select a group to see the distribution of aid")
 
 for dg in data_groups:
     fig_group = go.Figure()
@@ -76,5 +73,5 @@ for dg in data_groups:
     data.sort_values(by=['Value'], inplace=True, ascending=False)
     fig_group = go.Figure(data=go.Bar(x=data['Recipient'], y=data['Value']))
 
-    fig_group.update_layout(title=dg['acronym'], xaxis_title='Year', yaxis_title='USD, constant prices')
+    fig_group.update_layout(title=dg['acronym'], xaxis_title='Country', yaxis_title='USD, constant prices')
     st.plotly_chart(fig_group, use_container_width=True)
